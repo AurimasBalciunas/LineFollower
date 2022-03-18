493a451070424f175bd22e611646f3d32022665c
austinjad <amj102@pitt.edu>
2022-03-18 03:06:54 -0400

    Added FSM Functionality

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
+#include <stdio.h>
 #include "msp.h"
-
+#include "../inc/Clock.c" // Change these to .h
+#include "../inc/Motor.c"
+#include "../inc/SysTickInts.c"
+#include "FSMController.h"
+#include "Reflectance.h"
+#include "SensorInt.h"
 
 /**
  * main.c
  */
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    Clock_Init48MHz();
+    SysTick_Init(48000, 5); // Every 1ms, priority 5
+    start_fsm();
 }
