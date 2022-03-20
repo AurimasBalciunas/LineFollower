f14c60aa46ed7b1f19c197772346e1f70cebc0a6
Aurimas Balciunas <aurisb@Aurimass-MacBook-Pro.local>
2022-03-20 13:39:39 -0400

    Bump and FSM and Motors

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "msp.h"
+#include "../inc/CortexM.h"
 #include "../inc/Clock.h" // Change these to .h
 #include "../inc/Motor.h"
 #include "../inc/SysTickInts.h"
+#include "../inc/BumpInt.h"
 #include "FSMController.h"
 #include "Reflectance.h"
 #include "SensorInt.h"
 
+
+
+uint8_t CollisionData, CollisionFlag;  // mailbox
+void HandleCollision(uint8_t bumpSensor){
+   CollisionData = bumpSensor;
+   CollisionFlag = 1;
+   Motor_Stop();
+}
 
 /**
  * main.c
@@ -16,5 +26,11 @@ void main(void)
     Clock_Init48MHz();
     SysTick_Init(48000, 5); // Every 1ms, priority 5
     Motor_Init();
+    BumpInt_Init(&HandleCollision);
+    EnableInterrupts();
     start_fsm();
+
 }
+
+
+
